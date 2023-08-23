5ddde7666c24a87cbb0cf9cd8095e547ea11d483
zeng <510279293@qq.com>
2023-08-23 15:19:38 +0800

    add some

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd';
 import App from './App.tsx'
+import zhCN from 'antd/locale/zh_CN';
 import './assets/css/index.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ConfigProvider locale={zhCN}>
       <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
